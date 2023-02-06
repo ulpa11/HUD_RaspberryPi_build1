68510a31580ea991abbd8f3f33c6a69f2a591903
ulpa11 <ashutoshraghuwanshi28@gmail.com>
2023-02-06 17:59:35 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,28 +1,44 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
+import subprocess
+import sys
 
 # Create your views here.
 import subprocess
 from django.http import HttpResponse
+
 
 def login(request):
     if request.method == 'POST':
         if 'ssid' in request.POST and 'password' in request.POST:
             ssid = request.POST['ssid']
             password = request.POST['password']
+            wpa_supplicant_conf = f"""
+                country=US
+                ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+                update_config=1
+                network={{
+                ssid="{ssid}"
+                psk="{password}"
+                key_mgmt=WPA-PSK
+                }}
+                """
+            try:
+                with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as f:
+                    f.write(wpa_supplicant_conf)
+                subprocess.call(["wpa_cli", "-i", "wlan0", "reconfigure"])
+                subprocess.call(["dhclient", "wlan0"])
+            except Exception as e:
+                print("An error occurred while trying to connect to the Wi-Fi network.")
+                print(e)
 
-            conf = 'network={\n' \
-                   '    ssid="' + ssid + '"\n' \
-                   '    psk="' + password + '"\n' \
-                   '}\n'
+                ip_address = subprocess.check_output(["hostname", "-I"]).decode().strip()
+            if not ip_address:
+                print(
+                    "Could not retrieve IP address. Please check if the Wi-Fi credentials are correct and the network is in range.")
 
-            with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as f:
-                f.write(conf)
+            print("Connected to Wi-Fi network: ", ssid)
+            print("IP address: ", ip_address)
 
-            output = subprocess.call(['sudo', 'wpa_cli', '-i', 'wlan0', 'reconfigure'], shell=True)
-            if output == 0:
-                return HttpResponse("Wifi Connected")
-            else:
-                return HttpResponse("Failed to connect to wifi")
         else:
             return HttpResponse("Missing ssid or password")
     return render(request, 'login.html')
