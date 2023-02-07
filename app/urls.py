9fed4ce53db5796cc0623b164637426b07b584e0
ulpa11 <ashutoshraghuwanshi28@gmail.com>
2023-02-07 13:39:14 +0530

    append

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,10 @@
+#import path
+from django.urls import path
+from .views import wifi_names, login, reading_data,treatment_running
 
-from django.urls import path,include
-from .views import login,reading_data,treatment_running,wifi_names
-from django.conf.urls.static import static
-from django.conf import settings
 urlpatterns = [
-    path('',login,name="login"),
-    path('reading_data',reading_data,name="reading_data"),
-    path('treatment_running',treatment_running,name="treatment_running"),
-    path('wifi_names',wifi_names,name="wifi_names"),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', wifi_names, name='wifi_names'),
+    path('login/', login, name='login'),
+    path('reading_data/', reading_data, name='reading_data'),
+    path('treatment_running/', treatment_running, name='treatment_running'),
+]
--- app/views.py
+++ app/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render, redirect
 import subprocess
-from hardware  import call_function
 #import json responce
 from django.http import JsonResponse
 from django.http import HttpResponse
 
 
+from django.shortcuts import render, redirect
+import subprocess
 
-def button_callback(channel):
-    print("Button was pushed!")
-    state = True
+def wifi_names(request):
+    try:
+        result = subprocess.run(["iwlist", "wlan0", "scan"], stdout=subprocess.PIPE)
+        output = result.stdout.decode("utf-8")
+        ssid_list = []
+        for line in output.split("\n"):
+            if "ESSID:" in line:
+                ssid = line.split("ESSID:")[1].strip('"')
+                ssid_list.append(ssid)
+        if request.method == 'POST':
+            selected_ssid = request.POST.get('ssid')
+            return redirect('login', selected_ssid=selected_ssid)
+        return render(request, 'wifi_names.html', {'ssid_list': ssid_list})
+    except Exception as e:
+        print("An error occurred while trying to retrieve the Wi-Fi network names.")
+        print(e)
+        return HttpResponse("An error occurred while trying to retrieve the Wi-Fi network names.")
 
-def login(request):
+def login(request, selected_ssid):
     if request.method == 'POST':
-        if 'ssid' in request.POST and 'password' in request.POST:
-            ssid = request.POST['ssid']
+        if 'password' in request.POST:
             password = request.POST['password']
             wpa_supplicant_conf = f"""
                 country=US
                 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
                 update_config=1
                 network={{
-                ssid="{ssid}"
+                ssid="{selected_ssid}"
                 psk="{password}"
                 key_mgmt=WPA-PSK
                 }}
@@ -40,13 +54,49 @@ def login(request):
                 print(
                     "Could not retrieve IP address. Please check if the Wi-Fi credentials are correct and the network is in range.")
 
-            print("Connected to Wi-Fi network: ", ssid)
+            print("Connected to Wi-Fi network: ", selected_ssid)
             print("IP address: ", ip_address)
             return redirect('reading_data')
 
-        else:
-            return HttpResponse("Missing ssid or password")
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'selected_ssid': selected_ssid})
+
+
+# def login(request):
+#     if request.method == 'POST':
+#         if 'ssid' in request.POST and 'password' in request.POST:
+#             ssid = request.POST['ssid']
+#             password = request.POST['password']
+#             wpa_supplicant_conf = f"""
+#                 country=US
+#                 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+#                 update_config=1
+#                 network={{
+#                 ssid="{ssid}"
+#                 psk="{password}"
+#                 key_mgmt=WPA-PSK
+#                 }}
+#                 """
+#             try:
+#                 with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w") as f:
+#                     f.write(wpa_supplicant_conf)
+#                 subprocess.call(["wpa_cli", "-i", "wlan0", "reconfigure"])
+#                 subprocess.call(["dhclient", "wlan0"])
+#             except Exception as e:
+#                 print("An error occurred while trying to connect to the Wi-Fi network.")
+#                 print(e)
+
+#                 ip_address = subprocess.check_output(["hostname", "-I"]).decode().strip()
+#             if not ip_address:
+#                 print(
+#                     "Could not retrieve IP address. Please check if the Wi-Fi credentials are correct and the network is in range.")
+
+#             print("Connected to Wi-Fi network: ", ssid)
+#             print("IP address: ", ip_address)
+#             return redirect('reading_data')
+
+#         else:
+#             return HttpResponse("Missing ssid or password")
+#     return render(request, 'login.html')
 
 def reading_data(request):
     if request.method=="POST":
@@ -56,8 +106,10 @@ def reading_data(request):
 
 def treatment_running(request):
     #run call function
-    call_function()
     return render(request, 'treatment_running.html')
+
+
+                
 
 # def wifi_names(request):
 #     try:
@@ -68,24 +120,9 @@ def treatment_running(request):
 #             if "ESSID:" in line:
 #                 ssid = line.split("ESSID:")[1].strip('"')
 #                 ssid_list.append(ssid)
-#         return JsonResponse(ssid_list, safe=False)
+#         return render(request, 'wifi_names.html', {'ssid_list': ssid_list})
 #     except Exception as e:
 #         print("An error occurred while trying to retrieve the Wi-Fi network names.")
 #         print(e)
 #         return HttpResponse("An error occurred while trying to retrieve the Wi-Fi network names.")
-                
-
-def wifi_names(request):
-    try:
-        result = subprocess.run(["iwlist", "wlan0", "scan"], stdout=subprocess.PIPE)
-        output = result.stdout.decode("utf-8")
-        ssid_list = []
-        for line in output.split("\n"):
-            if "ESSID:" in line:
-                ssid = line.split("ESSID:")[1].strip('"')
-                ssid_list.append(ssid)
-        return render(request, 'wifi_names.html', {'ssid_list': ssid_list})
-    except Exception as e:
-        print("An error occurred while trying to retrieve the Wi-Fi network names.")
-        print(e)
-        return HttpResponse("An error occurred while trying to retrieve the Wi-Fi network names.")
+    
